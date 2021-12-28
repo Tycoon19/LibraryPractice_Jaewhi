cf52a2daa37b47abba65a19c23150b40e95c71ef
Tycoon19 <kevin960831@naver.com>
2021-12-28 23:28:16 +0900

    웹상의 이미지 불러오기 (Glide 활용)  - 인터넷 권한 획득 체험

--- app/src/main/java/com/example/librarypractice_jaewhi/MainActivity.kt
+++ app/src/main/java/com/example/librarypractice_jaewhi/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.librarypractice_jaewhi
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.bumptech.glide.Glide
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -16,6 +17,10 @@ class MainActivity : AppCompatActivity() {
             startActivity(myIntent)
 
         }
+
+//        인터넷에 있는 이미지(https:///...)를 이미지뷰에 바로 대입
+
+        Glide.with(this).load("https://i.ytimg.com/vi/qk8mlbBhHuY/maxresdefault.jpg").into(filmoImg)
 
     }
 }
