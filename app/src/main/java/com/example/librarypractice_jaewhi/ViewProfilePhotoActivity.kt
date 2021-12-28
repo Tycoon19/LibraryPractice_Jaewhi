9e0a2bcd1fd1e758b72a87390edb6fb644a0abff
Tycoon19 <kevin960831@naver.com>
2021-12-28 22:31:43 +0900

    프사 크게 보는 화면 추가

---
+++ app/src/main/java/com/example/librarypractice_jaewhi/ViewProfilePhotoActivity.kt
@@ -0,0 +1,11 @@
+package com.example.librarypractice_jaewhi
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ViewProfilePhotoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_view_profile_photo)
+    }
+}
